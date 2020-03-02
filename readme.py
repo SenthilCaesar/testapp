c85dda2b5661bc81696b3bd9b30c53227cb33138
senthilcaesar <senthilcaesar@yahoo.co.in>
2020-03-02 09:34:02 -0500

    files deleted

---
+++ readme.py
@@ -0,0 +1,51 @@
+# To create local repository
+
+mkdir newproject
+cd newproject
+
+# Next we need to initialize the repository
+
+git init
+
+# Next we need to add a file to the project
+
+touch readme.txt
+
+# Now you will have an empty file in your repository
+
+git status
+
+# Even though git is aware of the file, it has not been added to the project
+
+git add readme.txt
+git status
+
+# Your First commit, commit is a record of the file you have changed within the project
+# Before we do this, we have to inform Git who we are
+
+git config --global user.email senthilcaesar@yahoo.co.in
+git config --global user.name senthilcaesar
+
+# New we can create the commit
+
+git commit -m "test message"
+
+# Create a branch and push it to GitHub
+
+git checkout -b gamemodules
+git branch
+
+# Next we need to create a repository on GitHub, Click the new repository button from
+# your main page and click create repository
+# After creating the repository you will be presented with a URL to use for pushing our new created local repository
+
+git remote add origin https://github.com/SenthilCaesar/testapp
+git push -u origin master
+
+# Pulling the project
+# Say your collaborators make changes to the code on the GitHub project and have merged those changes
+
+git pull origin master
+
+# clone branch
+git clone -b remove-ext-dep https://github.com/pnlbwh/CNN-Diffusion-MRIBrain-Segmentation
